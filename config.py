e34cbab120a74ab8b2cf7e3dadc6586f2196fd7a
Mithun <76549104+Agent-150@users.noreply.github.com>
2024-03-10 20:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 # Url Shortner Information 
-SHORT = bool(environ.get('SHORT', True)) # Set False If you want shortlink off else True
+SHORT = bool(os.environ.get('SHORT', True)) # Set False If you want shortlink off else True
 URL = environ.get('URL', 'moneykamalo.com') # your shortlink url domain or url without https://
 API = environ.get('API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
 TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
